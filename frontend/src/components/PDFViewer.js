f65ae4f10e2507e2cf8fbd043632b95b52176601
Nino <nino.bachmann@pm.me>
2025-10-30 10:06:45 +0100

    .

--- frontend/src/components/PDFViewer.js
+++ frontend/src/components/PDFViewer.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 import { pdfjs } from 'pdfjs-dist';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
