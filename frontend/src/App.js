e7b299126f4bfb0cfc5df8788902a7a203815eb6
Nino <nino.bachmann@pm.me>
2025-10-30 16:57:33 +0100

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,6 @@
-/* import React, { useState } from 'react';
+import React, { useState } from 'react';
 import UploadPage from './pages/UploadPage';
 import AnnotatePage from './pages/AnnotatePage';
-
-function App() {
-  const [selectedPDF, setSelectedPDF] = useState(null);
-
-  return (
-    <>
-      <div>Hello, frontend works!</div> {/* <-- This ensures something is always visible *//*} 
-      {!selectedPDF
-        ? <UploadPage onSelectPDF={setSelectedPDF} />
-        : <AnnotatePage pdfDoc={selectedPDF} />
-      }
-    </>
-  );
-}
-
-export default App; */
 
 function App() {
   const [selectedPDF, setSelectedPDF] = useState(null);
