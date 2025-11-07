102b3feefcd178d92246aca055f201620b9b8d84
Nino <nino.bachmann@pm.me>
2025-11-07 11:09:19 +0100

    update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/api";
+const BASE_URL = process.env.REACT_APP_API_URL || "p-id-marker-production.up.railway.app";
 
 export const uploadPDF = (file, filename) => {
   const formData = new FormData();
