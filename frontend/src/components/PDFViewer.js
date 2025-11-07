ff2adcc181280ba9b9954c00a921e9ff58486072
Nino <nino.bachmann@pm.me>
2025-11-07 10:37:47 +0100

    .

--- frontend/src/components/PDFViewer.js
+++ frontend/src/components/PDFViewer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
+  'pdfjs-dist/build/pdf.worker.min.js',
   import.meta.url,
 ).toString();
 
