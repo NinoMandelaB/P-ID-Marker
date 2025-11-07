9811968990ba5a4207fab9cd0dce0a560793e052
Nino <nino.bachmann@pm.me>
2025-11-07 09:29:27 +0100

    .

--- frontend/src/pages/PDFViewer.js
+++ frontend/src/pages/PDFViewer.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Document, Page,PDFViewer } from 'react-pdf/renderer';
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf/build/pdf.js';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function PDFViewer({ pdfData, pageNum = 1, onPageChange }) {
   return (
     <div>
-      <Document file={{ data: pdfData }} onLoadSuccess={({ numPages }) => onPageChange(pageNum, numPages)}>
+      <Document
+        file={{ data: pdfData }}
+        onLoadSuccess={({ numPages }) => onPageChange(pageNum, numPages)}
+      >
         <Page pageNumber={pageNum} />
       </Document>
+
       <div>
-        <button disabled={pageNum <= 1} onClick={() => onPageChange(pageNum - 1)}>Prev</button>
+        <button disabled={pageNum <= 1} onClick={() => onPageChange(pageNum - 1)}>
+          Prev
+        </button>
         <button onClick={() => onPageChange(pageNum + 1)}>Next</button>
       </div>
     </div>
