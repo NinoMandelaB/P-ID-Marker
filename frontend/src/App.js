8c0062a5d92a3ee72358a24b95f8e97d3f4a1218
Nino <nino.bachmann@pm.me>
2025-11-07 12:35:19 +0100

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import AnnotatePage from './pages/AnnotatePage';
 
 function App() {
   const [selectedPDF, setSelectedPDF] = useState(null);
+  const handleGoBack = () => setSelectedPDF(null);
+
   return (
     <>
       <div>Hello, frontend works!</div>
       {!selectedPDF
         ? <UploadPage onSelectPDF={setSelectedPDF} />
-        : <AnnotatePage pdfDoc={selectedPDF} />
+        : <AnnotatePage pdfDoc={selectedPDF} goBack={handleGoBack} />
       }
     </>
   );
